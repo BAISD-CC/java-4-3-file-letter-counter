fb3fca91bc96f436015da1daa0edcc5700e7873b
Justin Kruskie <justin@kruskie.net>
2024-09-10 10:39:07 -0400

    Create FileLetterCounter.java

---
+++ FileLetterCounter.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class FileLetterCounter {
+
+    public static void main(String[] args) throws FileNotFoundException, java.io.IOException {
+        // Variables and keyboard scanner.
+
+        // Get the file name from the user.
+
+        // Close the keyboard 
+
+        // Make sure the letter is uppercase.
+
+        // Retrieve the letter as a character
+
+        // Open the file. We suggest FileReader and BufferedReader.
+
+        // Read the first line from the file.
+
+        // Process the entire file using a loop to check each line.
+            // Get the uppercase equivalent of the line.
+            // Use a second loop to check each character.
+                // Check to see if the current letter matches the desired letter.
+                    // Add one to the counter if it does.
+            // Read the next line.
+
+        // Close the file.
+
+        // Format and display the count.
+
+    }
+}
+
